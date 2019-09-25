7511ab5b409ea305ef0e369ec844699ee392ef16
Matthew C <m23.cullen@gmail.com>
2019-09-25 20:55:16 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,25 @@
 class BankAccount
-
+  attr_accessor :balance, :status 
+  attr_reader :name 
+  
+  def initialize(name)
+    @name = name 
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def deposit(amt)
+    self.balance += amt 
+  end 
+  
+  def display_balance
+    self.balance 
+  end 
+  
+  def valid?
+  end 
+  
+  def close_account
+  end 
+  
 end
