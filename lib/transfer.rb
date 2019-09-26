003f97b20902c2adf915fe0f7927eec750354155
Matthew C <m23.cullen@gmail.com>
2019-09-26 02:53:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -30,5 +30,10 @@ class Transfer
   end
   
   def reverse_transfer
+    if self.status == "complete"
+      self.sender.balance += amount
+      self.receiver.balance -= amount
+      self.status = "reversed"
+    end 
   end 
 end
